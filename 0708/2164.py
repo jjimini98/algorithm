cb8a46da3e3ed253618a3b685a74ce6ce53df1e4
jjimini98 <jjimini98@naver.com>
2021-07-09 18:05:28 +0900

    2164 진행중

--- 0708/2164.py
+++ 0708/2164.py
@@ -1,2 +1,21 @@
-lis = [1,2,3]
-print(lis.pop())
+from collections import deque 
+
+deq = deque() 
+
+for x in range(1, int(input())+1): 
+    deq.append(x)
+print("original deque : " , deq)
+
+
+print(deq.popleft())
+deq = deq.append(deq[0])
+print(deq[0])
+# while True:
+#     if len(deq) > 1 :
+#         deq.popleft()
+#         deq.append(deq[0])
+#         deq.remove(deq[0])
+#         print(deq)
+    
+#     elif len(deq) == 1:
+#         print(deq)
