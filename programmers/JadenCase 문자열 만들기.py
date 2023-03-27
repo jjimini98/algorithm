2f9d556636b139b1b2a80409434f166112f18338
Jimin <jjimini98@naver.com>
2023-03-27 21:38:20 +0900

    improve : 프로그래머스 Lv.2 JadenCase 문자열 만들기

--- programmers/JadenCase 문자열 만들기.py
+++ programmers/JadenCase 문자열 만들기.py
@@ -1,23 +1,15 @@
-from string import ascii_lowercase, ascii_uppercase
-
 def solution(s):
     answer = ''
-    
+
     s = s.lower().split(' ')
     
     for word in s:
         if word == '':
             answer += ' '
             continue
-
-        if word[0] in ascii_lowercase:
-            idx = ascii_lowercase.index(word[0])
-            answer += ascii_uppercase[idx]
-            answer += word[1:]
-        else:
-            answer += word
+        answer += word.capitalize()
         answer += ' '
-
+        
     return answer[:-1]
 
 
