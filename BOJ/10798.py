e28f420d5dffb78f8c926f38cb7bdffbd997ca53
Jimin <jjimini98@naver.com>
2024-08-14 18:24:24 +0900

    feat : solve BOJ 10798

--- BOJ/10798.py
+++ BOJ/10798.py
@@ -2,39 +2,6 @@ from sys import stdin
 
 input = stdin.readline
 
-# words = []
-# for _ in range(5):
-#     lis =list(input().split("\n")[0])
-#     words.append(lis)
-# # print(words)
-#
-# new_word = []
-# max_len = 0
-# for word in words:
-#     max_len = max(max_len, len(word))
-#     if len(word) < max_len:
-#         v = max_len - len(word)
-#         for _ in range(v):
-#             word += ' '
-#             new_word.append(word)
-#     else:
-#         new_word.append(word)
-# for d in range(len(word)):
-#     for word in new_word:
-
-
-# i = 0
-# for _ in
-#     for word in new_word:
-#         for i in range(len(word)):#
-#             result += word[i]
-#             break
-#     i += 1
-#
-# result = result.replace(' ', '')
-# print(result)
-
-
 def refine_maxlen(lis : list, max_len):
     nec = max_len-len(lis)
     for i in range(nec):
@@ -48,7 +15,7 @@ for _ in range(5):
 
 refine_words = []
 max_len = 0
-for word in words:
+for word in words: #여기서 뭔가 잘못된듯하다. def를 타지 않음
     if len(word) >= max_len:
         max_len = len(word)
         refine_words.append(word)
@@ -59,7 +26,7 @@ final_word = []
 for i in range(max_len):
     lis = []
     for word in refine_words:
-
+        print(word)
         try:
             lis.append(word[i])
         except:
