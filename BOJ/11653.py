5f992b7516fa953715a915725bf11779a35e388b
Jimin <jjimini98@naver.com>
2025-07-07 15:54:39 +0900

    solve : BOJ #11653 소인수분해

--- BOJ/11653.py
+++ BOJ/11653.py
@@ -1,33 +1,82 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# def is_prime_number(N):
+#     for i in range(2, N):
+#         if N % i == 0:
+#             return False  # 소수가 아님
+#     return True  # 소수임
+#
+# # def
+#
+#
+#
+# def main():
+#     N = int(input())
+#     number = 2
+#     while True  :
+#         isprime = is_prime_number(number)
+#         if isprime == True: # 소수면 최대한 나눠보고 넘어간다.
+#             while True:
+#                 if N % number != 0 :
+#                     number += 1
+#                     break
+#                 else:
+#                     N = N // number
+#                     print(number)
+#
+#         if N == 1:
+#             break
+#
+#
+# if __name__ == '__main__':
+#     main()
+from sys import stdin
+import math
 
-def is_prime_number(N):
-    for i in range(2, N):
-        if N % i == 0:
-            return False  # 소수가 아님
-    return True  # 소수임
-
-# def
 
 
+# 정해진 N 이라는 숫자 전까지 있는 모든 소수 구하기
 
-def main():
-    N = int(input())
-    number = 2
-    while True  :
-        isprime = is_prime_number(number)
-        if isprime == True: # 소수면 최대한 나눠보고 넘어간다.
-            while True:
-                if N % number != 0 :
-                    number += 1
-                    break
-                else:
-                    N = N // number
-                    print(number)
+def get_prime_number(N):
+    prime = []
 
-        if N == 1:
-            break
+    array = [True for _ in range(N+1)]
+
+    for i in range(2, int(math.sqrt(N))+1):
+        if array[i] == True:
+            j = 2
+            while i * j <= N:
+                array[i * j] = False
+                j += 1
+
+    for i in range(2,N+1):
+        if array[i]:
+            prime.append(i)
+
+    return prime
 
 
-if __name__ == '__main__':
-    main()
+def main(N):
+    answer = []
+    prime = get_prime_number(N)
+    for i in prime:
+        while True:
+            if N % i == 0:
+                answer.append(i)
+                N = N // i
+            else :
+                break
+
+    for an in sorted(answer) :
+        print(an)
+
+
+
+
+
+if __name__  == "__main__":
+    N = int(stdin.readline())
+    main(N)
+
+
